dece682f8143bf87c8008df9758defb7180e4d63
Keenonthedaywalker <53871946+Keenonthedaywalker@users.noreply.github.com>
2020-05-24 09:28:02 +0200

    C#MP3Player First Commit

---
+++ Music_Player_Forms_Edition/Music_Player_Forms_Edition/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Music_Player_Forms_Edition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.playButton = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(37, 12);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open...";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(367, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // playButton
+            // 
+            this.playButton.BackColor = System.Drawing.Color.Transparent;
+            this.playButton.Image = ((System.Drawing.Image)(resources.GetObject("playButton.Image")));
+            this.playButton.Location = new System.Drawing.Point(12, 347);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(220, 91);
+            this.playButton.TabIndex = 2;
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.BackColor = System.Drawing.Color.Transparent;
+            this.pauseButton.Image = ((System.Drawing.Image)(resources.GetObject("pauseButton.Image")));
+            this.pauseButton.Location = new System.Drawing.Point(238, 347);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(185, 91);
+            this.pauseButton.TabIndex = 3;
+            this.pauseButton.UseVisualStyleBackColor = false;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Image = ((System.Drawing.Image)(resources.GetObject("stopButton.Image")));
+            this.stopButton.Location = new System.Drawing.Point(429, 347);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(173, 91);
+            this.stopButton.TabIndex = 4;
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.openButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.BindingSource bindingSource1;
+    }
+}
+
---
+++ Music_Player_Forms_Edition/Music_Player_Forms_Edition/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Music_Player_Forms_Edition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Creates Media Player
+        WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+
+        //Creates Openfiledialog
+        OpenFileDialog dlg = new OpenFileDialog();
+
+        SoundPlayer splayer = new SoundPlayer();
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //A filter if you only want to display specific files.
+            //dlg.Filter = "MP3|*.mp3";
+
+            //Will only continue with the code if user selects a file and presses ok.
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+
+                textBox1.Text = dlg.FileName;
+
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+
+            wplayer.URL = textBox1.Text;
+            wplayer.controls.play();
+
+        }
+
+        bool isPaused = true;
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+
+            if (isPaused)
+            {
+                wplayer.controls.pause();
+
+                isPaused = false;
+
+                pauseButton.Image = Image.FromFile("C:\\Users\\keeno\\source\\repos\\Music_Player_Forms_Edition\\images\\play_button.png");
+
+            } else
+            {
+                wplayer.controls.play();
+
+                isPaused = true;
+
+                pauseButton.Image = Image.FromFile("C:\\Users\\keeno\\source\\repos\\Music_Player_Forms_Edition\\images\\pause_button.png");
+            }
+           
+
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            wplayer.controls.stop();
+        }
+
+
+       /* private string TimeFormat(int iMilliseconds)
+        {
+
+            int iTotalseconds = iMilliseconds / 1000;
+            int hours = (iTotalseconds >= 3600) ? iTotalseconds / 3600 : 0;
+            int hours_res = (iTotalseconds >= 3600) ? iTotalseconds - (hours * 3600) : iTotalseconds;
+            int minutes = hours_res / 60;
+            int minutes_res = hours_res - (minutes * 60);
+            int seconds = minutes_res % 60;
+            string strTimeString = String.Format("{0,1}:{1,2:D2}:{2,2:D2}", hours, minutes, seconds);
+            return strTimeString;
+
+        } */
+
+    }
+}
